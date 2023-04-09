0a92e76827a224a3d13b4bd2ec5010f8bb824528
Ubuntu <ubuntu@ubuphy5.home.arpa>
2023-04-09 16:19:09 +0000

    20230409

--- init.sh
+++ init.sh
@@ -1,15 +1,26 @@
-#!/bin/zsh
+# Launch all machines needed for a kuberenetes tutorial environment
+# Author: Collin O'Neill <collin@pobhox.com>
+
+#!/bin/bash
+
+# Variables
 NEWPASSWD=ubuntu:Excretory-Overpower-Tuition6-Antiquity
 
-mulitpass list
-for machine in kansible1 kcontrol1 kworker1 kworker2 kworker3; do
-    multipass launch --name $machine;
-    multipass stop $machine;
-    multipass set local.$machine.cpus=2;
-    multipass set local.$machine.memory=2G;
-    multipass start $machine;
+# Action
+multipass list
+
+for machine in kansible1 kcontrol1 kcontrol2; do
+    multipass launch --name $machine -c 1 -d 8G -m 1G;
+done
+
+for machine in kworker1 kworker2 kworker3; do
+    multipass launch --name $machine -c 4 -d 30G -m 4G;
+done
+
+for machine in kansible1 kcontrol1 kcontrol2 kworker1 kworker2 kworker3; do
     echo $NEWPASSWD | multipass exec $machine -- sudo /usr/sbin/chpasswd
     multipass exec $machine -- sudo /usr/bin/sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-    multipass exec $machine -- sudo /usr/bin/systemctl restart ssh
+    multipass exec $machine -- sudo /usr/bin/systemctl restart ssh;
 done
+
 multipass list
--- purge.sh
+++ purge.sh
@@ -1,10 +1,12 @@
-#!/bin/zsh
+#!/bin/bash
 
 multipass list
+
 for machine in kansible1 kcontrol1 kworker1 kworker2 kworker3; do
     multipass stop $machine;
     multipass delete $machine;
     multipass purge;
 done
+
 multipass list
 
